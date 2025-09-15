faa46895f2348543cc8763cc91cbd4dc2d8e44c0
joc135 <joc135@ucsd.edu>
2025-09-15 16:52:29 -0700

    Added script to get SPY data from PostgreSQL to CSV

---
+++ export_to_csv.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+import os
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+df = pd.read_sql(text("SELECT * FROM stock_data WHERE symbol='SPY'"), engine)
+df.to_csv("spy_data.csv", index=False)
+print("SPY data exported to spy_data.csv")

d01f4c2348be497ed38299d673c44bcf366b2f28
joc135 <joc135@ucsd.edu>
2025-09-15 17:19:24 -0700

    Incorrect where condition in sql. Fixed so that it gets data for each ticker in the S&P500

--- export_to_csv.py
+++ export_to_csv.py
@@ -3,6 +3,6 @@ from sqlalchemy import create_engine, text
 import os
 
 engine = create_engine(os.getenv("DATABASE_URL"))
-df = pd.read_sql(text("SELECT * FROM stock_data WHERE symbol='SPY'"), engine)
+df = pd.read_sql(text("SELECT * FROM stock_data"), engine)
 df.to_csv("spy_data.csv", index=False)
 print("SPY data exported to spy_data.csv")
